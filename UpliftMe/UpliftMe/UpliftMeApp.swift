1d3f696575ceec7484a41102f95486a8edd1920a
syousafz <153879678+syousafz@users.noreply.github.com>
2024-01-05 16:52:09 -0500

    added files

---
+++ UpliftMe/UpliftMe/UpliftMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpliftMeApp.swift
+//  UpliftMe
+//
+//  Created by Shabano Yousafzai on 2024-01-05.
+//
+
+import SwiftUI
+
+@main
+struct UpliftMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
